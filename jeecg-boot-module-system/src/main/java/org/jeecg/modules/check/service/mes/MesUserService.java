7fd09c2a76165dcd2755c98d69e51ce22a75fb18
zhangrui05 <zhangrui05@qiyi.com>
2022-10-07 17:18:38 +0800

    优化

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/check/service/mes/MesUserService.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/check/service/mes/MesUserService.java
@@ -42,29 +42,14 @@ public class MesUserService {
     }
 
     public String getUserCookie() {
-        return ".AspNetCore.Cookies=CfDJ8FAok77Wx-VAgWHfwSImD2QY8h-d1RCYIfK8xJsv5UaE_ihDyvxOaVyW5ZkR2yvxmL646lz0s-9mUksYdQEqx8oejvwli5brA3VUdvWLHBlV0MtR5xNEzL6XiAthdhtFLkts9sq3uKC79NZYp21mlpe58p9ZpOvoRwHTFcQ_pf-eDVip9wK30oXPzCKvcF2gnt7S7K2m3GSIFGsmRQYYF5kyCXXg71hwGCsjEAc9v1A5VSg9yLWJBKhINerLTM0JtYwMU-yLbfsOYPGOZMGaTuVdJtZeIJAYXkxRsOtV4OvQhWB-2-7IoLN4oEnTOupM5W66A5zjneEEmMXYv9qzXx0Leck7UeNgLjckc54rtOQdzQcW5cp-wWA7uPI6VqIbAV-WdqcOMUgdg4jvejW6nJQDXDksN5YPsVdHfHsrtDLwsVle5CPqFCpLMdeOXowIvyDktQqWUl7QEWZ6ulUGajA";
-/*        Map<String, Object> param = new HashMap<>();
-        param.put("username", "admin");
-        param.put("password", "maple");
-        HttpResponse response = HttpRequest.post("http://172.16.10.2:5000/Account/MESCheckLogin")
-                .contentType(ContentType.FORM_URLENCODED.getValue())
-                .form(param).execute();
-        return response == null ? null : response.getCookieStr();*/
-    }
-
-    public Map<String, Object> getCookie() {
         Map<String, Object> param = new HashMap<>();
         param.put("strUserCode", "admin");
         param.put("strPassword", "maple");
         HttpResponse response = HttpRequest.post("http://qzqms.check.com/Account/MESCheckLogin")
                 .contentType(ContentType.FORM_URLENCODED.getValue())
                 .form(param).execute();
-        String cookieStr = response.getCookieStr();
-        List<HttpCookie> cookies = response.getCookies();
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("cookieStr", cookieStr);
-        result.put("cookies", cookies);
-        return result;
+        log.info("MESCheckLogin return Cookie:{}",response.getCookieStr());
+        return response.getCookieStr();
     }
 
 }
